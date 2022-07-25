e2d2e087d579b649f56dfc6853627c3dbeb21e5e
Ayush Jain <74196641+ayushjain27@users.noreply.github.com>
2022-07-25 10:23:55 +0530

    Update Donation.jsx
    Updated

--- src/Components/Home/Donation.jsx
+++ src/Components/Home/Donation.jsx
@@ -44,7 +44,7 @@ const Donation = () => {
             <div className={`${styles.cards} card`}>
               <img src="https://www.gannett-cdn.com/-mm-/3b8b0abcb585d9841e5193c3d072eed1e5ce62bc/c=0-30-580-356/local/-/media/2017/10/05/USATODAY/usatsports/glass-jar-full-of-cois-with-donate-written-on-it-charity-donation-philanthropy_large.jpg?width=1200&disable=upscale&format=pjpg&auto=webp" className="card-img-top" alt="..." />
               <div className={`${styles.card} card-body`}>
-                <h5 className={`${styles.title} card-title`}>FOOD</h5>
+                <h5 className={`${styles.title} card-title`}>MONEY</h5>
               </div>
             </div>
           </div>
