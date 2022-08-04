175381b4be09db79075ea2f5a292b524f9a533f6
ayushjain27 <ayushjain201574@gmail.com>
2022-08-04 14:14:58 +0530

    Updated

--- src/Components/Items/Items.jsx
+++ src/Components/Items/Items.jsx
@@ -4,7 +4,17 @@ import styles from "./Items.module.css";
 const Items = () => {
   return (
     <>
-      Ayush
+      <div className="container text-center">
+          <div className="mt-2 mb-2 fs-2 fw-bold">Your Items</div>
+        <div className="row">
+          <div className="col-md-6">One of three columns</div>
+          <div className="col-md-6">One of three columns</div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">One of three columns</div>
+          <div className="col-md-6">One of three columns</div>
+        </div>
+      </div>
     </>
   );
 };
